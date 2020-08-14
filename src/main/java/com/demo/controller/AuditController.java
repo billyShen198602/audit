b4a7ea3c8690c236020b503233569c0661dcc58a
billy198602 <813688951@qq.com>
2020-08-14 13:47:43 +0800

    审批controller

--- src/main/java/com/demo/controller/AuditController.java
+++ src/main/java/com/demo/controller/AuditController.java
@@ -18,11 +18,21 @@ public class AuditController {
 
     @ApiOperation(value = "发起审批接口",notes = "用户发起审批功能")
     @CrossOrigin(origins = "*", maxAge = 3600)
-    @PostMapping("/audit")
+    @PostMapping("/promoter")
     public Result promoterTask(@RequestParam("promoterUserId") String promoterUserId,
                                @RequestParam("ruleId") String ruleId, @RequestParam("taskName") String taskName){
         String promoterTask = auditService.promoterTask(promoterUserId, ruleId, taskName);
         return ResultUtils.success(promoterTask);
+    }
+
+    @ApiOperation(value = "审批接口",notes = "用户审批功能")
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    @PostMapping("/audit")
+    public Result auditTask(@RequestParam("currentUserId") String currentUserId,@RequestParam("taskId") String taskId,
+                            @RequestParam("hasNextAudit") int hasNextAudit){
+        boolean flag = (hasNextAudit == 0)? false : true;
+        String auditTask = auditService.auditTask(currentUserId, taskId, flag);
+        return ResultUtils.success(auditTask);
     }
 
 
